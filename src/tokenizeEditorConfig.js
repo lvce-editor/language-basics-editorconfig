28d9e5369f42840c499dbe17f500ab0ad053c071
Le Vivilet <levivlet@pm.me>
2022-09-13 22:36:19 +0200

    highlight key value pairs

--- src/tokenizeEditorConfig.js
+++ src/tokenizeEditorConfig.js
@@ -4,6 +4,8 @@
 export const State = {
   TopLevelContent: 1,
   InsideLineComment: 2,
+  AfterPropertyName: 3,
+  AfterPropertyNameAfterEqualSign: 4,
 }
 
 export const StateMap = {
@@ -23,24 +25,36 @@ export const TokenType = {
   Comment: 885,
   Query: 886,
   Text: 887,
+  LanguageConstant: 11,
+  Numeric: 15,
+  PropertyName: 12,
+  PropertyValueString: 14,
+  Punctuation: 13,
+  String: 188,
 }
 
 export const TokenMap = {
-  [TokenType.CssSelector]: 'CssSelector',
-  [TokenType.Whitespace]: 'Whitespace',
-  [TokenType.None]: 'None',
-  [TokenType.Unknown]: 'Unknown',
-  [TokenType.NewLine]: 'NewLine',
   [TokenType.Comment]: 'Comment',
+  [TokenType.LanguageConstant]: 'LanguageConstant',
+  [TokenType.NewLine]: 'NewLine',
+  [TokenType.None]: 'None',
+  [TokenType.Numeric]: 'Numeric',
+  [TokenType.PropertyName]: 'JsonPropertyName',
   [TokenType.Query]: 'Query',
   [TokenType.Text]: 'Text',
+  [TokenType.Unknown]: 'Unknown',
+  [TokenType.Whitespace]: 'Whitespace',
+  [TokenType.Punctuation]: 'Punctuation',
+  [TokenType.PropertyValueString]: 'String',
+  [TokenType.String]: 'String',
 }
 
+const RE_LANGUAGE_CONSTANT = /^(?:true|false)/
 const RE_LINE_COMMENT_START = /^#/
 const RE_WHITESPACE = /^ +/
 const RE_CURLY_OPEN = /^\{/
 const RE_CURLY_CLOSE = /^\}/
-const RE_PROPERTY_NAME = /^[a-zA-Z\-]+\b/
+const RE_PROPERTY_NAME = /^[a-zA-Z\-\_\d\s]+\b(?=\s*\=)/
 const RE_COLON = /^:/
 const RE_PROPERTY_VALUE = /^[^;\}]+/
 const RE_SEMICOLON = /^;/
@@ -62,6 +76,7 @@ const RE_STAR = /^\*/
 const RE_QUERY_NAME = /^[a-z\-]+/
 const RE_QUERY_CONTENT = /^[^\)]+/
 const RE_COMBINATOR = /^[\+\>\~]/
+const RE_EQUAL_SIGN = /^\=/
 
 export const initialLineState = {
   state: State.TopLevelContent,
@@ -91,6 +106,10 @@ export const tokenizeLine = (line, lineState) => {
         } else if ((next = part.match(RE_WHITESPACE))) {
           token = TokenType.Whitespace
           state = State.TopLevelContent
+        } else if ((next = part.match(RE_PROPERTY_NAME))) {
+          part
+          token = TokenType.PropertyName
+          state = State.AfterPropertyName
         } else if ((next = part.match(RE_ANYTHING))) {
           token = TokenType.Text
           state = State.TopLevelContent
@@ -102,6 +121,38 @@ export const tokenizeLine = (line, lineState) => {
       case State.InsideLineComment:
         if ((next = part.match(RE_ANYTHING))) {
           token = TokenType.Comment
+          state = State.TopLevelContent
+        } else {
+          throw new Error('no')
+        }
+        break
+      case State.AfterPropertyName:
+        if ((next = part.match(RE_EQUAL_SIGN))) {
+          token = TokenType.Punctuation
+          state = State.AfterPropertyNameAfterEqualSign
+        } else if ((next = part.match(RE_WHITESPACE))) {
+          token = TokenType.Whitespace
+          state = State.AfterPropertyName
+        } else {
+          part
+          throw new Error('no')
+        }
+        break
+      case State.AfterPropertyNameAfterEqualSign:
+        if ((next = part.match(RE_WHITESPACE))) {
+          token = TokenType.Whitespace
+          state = State.AfterPropertyNameAfterEqualSign
+        } else if ((next = part.match(RE_LANGUAGE_CONSTANT))) {
+          token = TokenType.LanguageConstant
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_NUMERIC))) {
+          token = TokenType.Numeric
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_LINE_COMMENT_START))) {
+          token = TokenType.Comment
+          state = State.InsideLineComment
+        } else if ((next = part.match(RE_ANYTHING))) {
+          token = TokenType.PropertyValueString
           state = State.TopLevelContent
         } else {
           throw new Error('no')
